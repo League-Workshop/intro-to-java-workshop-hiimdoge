af53bfbd48de08c99e882ae96cab73fc74faabf4
League Student <league@iMac-5.attlocal.net>
2019-11-25 15:27:20 -0800

    Update MyFirstJavaProgram.java

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -7,9 +7,23 @@ public class MyFirstJavaProgram {
 	
 	public static void main(String[] args) {
 		
-		// START HERE
-	
-		
+		Robot toby = new Robot();
+		toby.sparkle();
+		toby.penDown();
+		toby.setSpeed(100);
+		for(int i = 0; i < 4; i ++ ) {
+			toby.move(100);
+		    toby.turn(90);
+		}
+		toby.unSparkle();
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		toby.penUp();
+		toby.move(500);
 		
 	}
 }
