3e10758be68147ce7083a606ea6f37bdd24875e8
League Student <league@iMac-5.attlocal.net>
2019-11-26 15:26:40 -0800

    Day 3

--- src/section3/WorldDomination.java
+++ src/section3/WorldDomination.java
@@ -6,7 +6,15 @@ public class WorldDomination {
 	
 	public static void main(String[] args) {
 		// 1. Ask the user if they know how to write code.
+		String codeYesNo = JOptionPane.showInputDialog("Do you know how to write code???");
+		if (codeYesNo.equalsIgnoreCase("yes")) {
+			JOptionPane.showMessageDialog(null, "US CODERS WILL RULE THE WORLD!!!!!!!");
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "have fun washing dishes lol...");
+		  } 
 
+			
 		// 2. If they say "yes", tell them they will rule the world.
 
 		// 3. Otherwise, wish them good luck washing dishes.
