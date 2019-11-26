03af0ddb036f430a506194a272812424b3140822
League Student <league@iMac-5.attlocal.net>
2019-11-26 14:15:23 -0800

    Day 2

--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -6,28 +6,38 @@ import org.jointheleague.graphical.robot.Robot;
 public class FourSquare {
 	
 	// 2. Create a new Robot
-
+	Robot toby = new Robot();
 	void go() {
 		// 4. Make the robot move as fast as possible
-
+		toby.setSpeed(100);
 		// 5. Set the pen width to 5
-
+        toby.setPenWidth(5);
 		// 6. Use a for loop to repeat steps #7 to #8, four times...
-
+        for(int i = 0; i < 4; i ++); {
 			// 7. Set the pen color to random
-	
-			// 1. Call the drawSquare() method
-	
+	        toby.setRandomPenColor();
 			// 8. Turn the robot 90 degrees to the right
+        	toby.turn(90);
+        }
+			// 1. Call the drawSquare() method
+	        drawSquare();
 
 	}
 
 	
 	void drawSquare() {
-		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
 		/* 3. Fill in the code to draw a square inside the method below. */
-		
-		
+		toby.sparkle();
+		toby.penDown();
+		toby.setSpeed(100);
+		for(int i = 0; i < 4; i ++ ) {
+			toby.move(100);
+		    toby.turn(90);
+		    
+		}
+		toby.penUp();
+		toby.unSparkle();
+		toby.move(1000);
 	}
 
 	public static void main(String[] args) {
